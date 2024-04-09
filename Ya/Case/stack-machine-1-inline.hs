95d34c2f04cbc00a11282c9a71d425fa89185ce5
Murat Kasimov <iokasimov.m@gmail.com>
2024-04-09 09:53:40 +0700

    Add inlined version of a case for stack machines

--- Ya/Case/stack-machine-1-inline.hs
+++ Ya/Case/stack-machine-1-inline.hs
@@ -23,26 +23,22 @@ pattern Operation x = That x :: Command v
 
 type Processing v = State `TI` List v `JT` Halts `TI` v
 
-load :: Immediate v `ARR` Processing v
-load v = enter
-	`yukl` push @List v `u` State
-
-eval :: Operation v `ARR` Processing v
-eval op = enter
-	`yukl` pop @List `u` State `u` try
-	`lm_dp` pop @List `u` State `u` try
-	`yokl` op `o` push @List `o` State
-
-main = Nonempty @List `a` Construct
+main = (Nonempty @List `a` Construct
  `a` Next (Immediate 1)
  `a` Next (Immediate 2)
  `a` Next (Operation ((+) `j_`))
  `a` Next (Immediate 4)
  `i` Last (Operation ((+) `j_`))
- `yoklKL` Forwards
-   `aaaa` Immediate `v` load
-     `rf` Operation `v` eval
- `rwwwww_rw` Empty @List ()
- `u_u_u_u_u_u` is "[ERROR] No operands!" `o` putStr
-   `yi_rf` pass `aaa` is "[OK] Traced: " `o` putStr
-   `cn_dp` (`yoklKL` Forwards `aaa` show `o` putStr `o_yukl` putChar ' ')
+
+ `yi_yoklKL` Forwards @(State `TI` List Int `JT` Halts)
+  `aaaaaaa` Immediate
+    `vvvv` intro @Int
+    `oo_yokl` push @List `o` State @(List Int)
+   `yi_yi_rf`  Operation
+    `vvvvv` pop @List `u` State `u` try
+     `lm_dp` pop @List `u` State `u` try
+     `yokl_a` push @List `o` State @(List Int)
+  ) `rwwwww_rw` Empty @List ()
+  `u_u_u_u_u_u` is "[ERROR] No operands!" `o` putStr
+    `yi_rf` pass `aaa` is "[OK] Traced: " `o` putStr
+    `cn_dp` (`yoklKL` Forwards `aaa` show `o` putStr `o_yukl` putChar ' ')
