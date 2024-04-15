3ba9e6bdabd4d6775e004c9c7730ba67594ff57c
Murat Kasimov <iokasimov.m@gmail.com>
2024-04-15 19:33:54 +0700

    Refactor task-manager-3

--- Ya/Case/task-manager-3.hs
+++ Ya/Case/task-manager-3.hs
@@ -3,8 +3,12 @@ import Ya
 import "base" Data.Char (Char)
 import "base" Data.String (String)
 import "base" Data.List ((++))
-import "base" System.IO (IO, getChar, putStrLn)
+import "base" System.IO (IO, getChar, putStrLn, print)
+
+import "ya-ascii" Ya.ASCII
+import "ya-expo" Ya.Expo.ASCII
 import "ya-expo" Ya.Expo.Instances ()
+
 import "ya-expo" Ya.Expo.Terminal as Terminal
 
 type Status = () `ML` ()
@@ -12,7 +16,7 @@ type Status = () `ML` ()
 pattern TODO = This () :: Status
 pattern DONE = That () :: Status
 
-type Task = Status `LM` String
+type Task = Status `LM` List ASCII
 
 pattern Task mark title = These mark title :: Task
 
@@ -21,11 +25,28 @@ type Bullet = () `ML` ()
 pattern Bullet = This () :: Bullet
 pattern Cursor = That () :: Bullet
 
-line :: Bullet -> Task `ARR` String
-line bullet (These status title) =
-	is "  - " `rf` is " -> " `yi` bullet ++ 
-	is "TODO" `rf` is "DONE" `yi` status ++
-	" " ++ title
+line :: List ASCII -> Task -> IO ()
+line prefix (Task status title) =
+ prefix `yoklKL` output `o` Forwards
+ `yi_yi_yukl` as @(List ASCII)
+   `yiiiii` is "TODO " `rf` is "DONE " `yi` status
+   `yoklKL` output `o` Forwards
+ `yi_yi_yukl` title `yoklKL` output `o` Forwards
+ `yi_yi_yukl` Newline `u` Signal `u` output
+
+refresh = Terminal.prepare
+     `yukl` Terminal.clear
+     `yukl` Newline `u` Signal `u` output
+
+render = as @(Scrolling List Task)
+ `oooo` (`yoklKL` line "    " `o` Backwards)
+ `cn_dp` (`yoklKL` line " -> ")
+ `cn_dp` (`yoklKL` line "    " `o` Forwards)
+
+handle :: Command -> State `TI` Scrolling List Task `TI` ()
+handle = pass `a` State `aaa` scroll @List @Task
+    `yi_rf` pass `a` State `aaa` switch
+     `oo_a` part @Focused `o` has @Task `o` has @Status
 
 type Command = Vertical `ML` Status
 
@@ -40,17 +61,14 @@ keypress 'D' = Some `i` Mark DONE
 keypress _ = None ()
 
 main = forever `o` unwrap `o` unwrap
-	`yi_yi_yi_yi` enter @(State `TI` Scrolling List Task `JT` IO)
-		`yi_yukl` Terminal.prepare `yukl` Terminal.clear
-		`yi_yukl` review `u` State @(Scrolling List Task)
-		`yi_yokl` (`yoklKL` line Bullet `o` putStrLn `o` Backwards)
-			`cn_dp` (`yoklKL` line Cursor `o` putStrLn)
-			`cn_dp` (`yoklKL` line Bullet `o` putStrLn `o` Forwards)
-		`yi_yukl` until `yii` getChar `yo` keypress
-		`yi_yokl` pass `a` State `aaa` scroll @List @Task
-			`yi_rf` pass `a` State `aaa` switch `oo_a` part @Focused `o` has @Task `o` has @Status
-	`yi_yi_yi_yi` transform `a` (Construct `o` Nonempty @List)
-		`aaaaaa` Next `yi` Task TODO "Apply to that new position"
-		`aaaaaa` Next `yi` Task TODO "Find a way to fix ligatures"
-		`aaaaaa` Next `yi` Task TODO "Organize a boardgame session"
-		`yiiiii` Last `yi` Task DONE "Buy a water gun for Songkran"
+ `yi_yi_yi_yi` enter @(State `TI` Scrolling List Task `JT` IO)
+   `yi_yukl` refresh
+   `yi_yukl` review `u` State
+   `yi_yokl` render
+   `yi_yukl` until `yii` getChar `yo` keypress
+   `yi_yokl` handle
+ `yi_yi_yi_yi` transform `aaa` Construct `o` Nonempty @List
+   `aaaaaa` Next `yi` Task TODO "Apply to that new position"
+   `aaaaaa` Next `yi` Task TODO "Find a way to fix ligatures"
+   `aaaaaa` Next `yi` Task TODO "Organize a boardgame session"
+   `yiiiii` Last `yi` Task DONE "Buy a water gun for Songkran"
