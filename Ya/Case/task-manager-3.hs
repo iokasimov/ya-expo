792c49278d4558e939a29c923a89d09a66e64f66
Murat Kasimov <iokasimov.m@gmail.com>
2024-07-18 20:45:12 +0700

    Update 3rd part of task-manager tutorial

--- Ya/Case/task-manager-3.hs
+++ Ya/Case/task-manager-3.hs
@@ -1,77 +1,58 @@
 import Ya
 
-import "base" Data.Char (Char)
-import "base" Data.String (String)
-import "base" Data.List ((++))
-import "base" System.IO (IO, getChar, putStrLn, print)
-
+import "base" System.IO (IO)
+import "ya-expo" Ya.Expo.Instances ()
 import "ya-ascii" Ya.ASCII
 import "ya-expo" Ya.Expo.ASCII
-import "ya-expo" Ya.Expo.Instances ()
 
 import "ya-expo" Ya.Expo.Terminal as Terminal
 
 type Status = () `ML` ()
 
+type Title = List ASCII
+
 pattern TODO = This () :: Status
 pattern DONE = That () :: Status
 
-type Task = Status `LM` List ASCII
-
-pattern Task mark title = These mark title :: Task
-
-type Bullet = () `ML` ()
-
-pattern Bullet = This () :: Bullet
-pattern Cursor = That () :: Bullet
-
-line prefix (Task status title) =
- prefix `u` is @(List ASCII)
-   `yoklKL` output `o` Fore
- `yi_yi_yukl` is @(List ASCII)
-   `yiiiii` but "TODO " `rf` but "DONE " `yi` status
-   `yoklKL` output `o` Fore
- `yi_yi_yukl` title `yoklKL` output `o` Fore
- `yi_yi_yukl` Newline `u` Signal `u` output
-
-refresh = Terminal.prepare
-     `yukl` Terminal.clear
-     `yukl` Newline `u` Signal `u` output
-
-render = is @(Scrolling List Task)
-    `o` unwrap @Arrow @(Scrolling List Task)
-    `o` unwrap @Arrow @((U_T_I_TT_I LM Only (Situation List)) Task)
- `oooo` (`yoklKL` line " -> ")
- `cn_dp` (unwrap @Arrow @((U_I_I LM `T_TT_I` List) Task) `o` unwrap @Arrow `ooo` (`yoklKL` line "    " `o` Back) `cn_dp` (`yoklKL` line "    " `o` Fore))
-
-handle :: Command -> State `TI` Scrolling List Task `TI` ()
-handle = pass `a` State `aaa` scroll @List @Task
-    `yi_rf` pass `a` State `aaa` switch `oo_a` sub @Focused `o'` has @Status
+type Task = Status `LM` Title
 
 type Command = Vertical `ML` Status
 
 pattern Move v = This v :: Command
 pattern Mark s = That s :: Command
 
-command :: ASCII `ARR` Optional Command
-command (Letter (Lowercase J)) = Some `yi` Move `i` Up ()
-command (Letter (Lowercase K)) = Some `yi` Move `i` Down () 
-command (Letter (Uppercase T)) = Some `yi` Mark `i` TODO
-command (Letter (Uppercase D)) = Some `yi` Mark `i` DONE
-command _ = None ()
+line point status title =
+ on @List point `yoklKL` Fore `a` output
+ `yi_yi_yukl` but "TODO " `rf` but "DONE " `yi` status `uu` on @List
+   `yoklKL` Fore `a` output
+ `yi_yi_yukl` title `yoklKL` Fore `a` output
+ `yi_yi_yukl` Newline `u` Signal `u` output
 
--- command :: ASCII `ARR` Optional Command
--- command = match @Letter `a_a` match @
+pressed k p =
+ k `e` p `yui` () `yiu` ()
 
-main = forever `o` unwrap @Arrow `o` unwrap @Arrow
- `yi_yi_yi_yi` enter @(State `TI` Scrolling List Task `JT` IO)
-   `yi_yukl` refresh
-   `yi_yukl` review `u` State
-   `yi_yokl` render
-   `yi_yukl` input `yo` command `uuu` until
-   `yi_yokl` handle
- `yi_yi_yi_yi` transform `aaa` Construct `o` Nonempty @List
-   `aaaaa` Next `yi` Task TODO "Apply to that new position"
-   `aaaaa` Next `yi` Task TODO "Find a way to fix ligatures"
-   `aaaaa` Next `yi` Task TODO "Organize a boardgame session"
-   `yiiiii` Last `yi` Task DONE "Buy a water gun for Songkran"
+key x = on @Optional
+ `yi_yi_yi` J `u` Lowercase `u` Letter `u` pressed x `u` Maybe
+   `lm_ds` K `u` Lowercase `u` Letter `u` pressed x `u` Maybe
+ `yi_lm_ds` T `u` Uppercase `u` Letter `u` pressed x `u` Maybe
+   `lm_ds` D `u` Uppercase `u` Letter `u` pressed x `u` Maybe
+
+initial = is @Task
+ `yi_yi` TODO `lm` is @Title `i` "Apply to that new position"
+ `yi_lm` DONE `lm` is @Title `i` "Find a way to fix ligatures"
+ `yi_lm` TODO `lm` is @Title `i` "Organize a boardgame session"
+ `yi_lm` DONE `lm` is @Title `i` "Buy a water gun for Songkran"
+
+main = forever
+ `yi_yi_yi` enter @(State `TI` Scrolling List Task `JT` IO)
+   `yukl` Terminal.prepare `yukl` Terminal.clear
+   `yukl` State `yii` review `aa` sub @(Situation List) `o'` rep Backwards
+     `yokl_yoklKL` (line "  - " `j'`) `o` Back
+   `yukl` State `yii` review `aa` sub @Focused
+     `yokl_yoklKL` (line " -> " `j'`)
+   `yukl` State `yii` review `aa` sub @(Situation List) `o'` rep Forwards
+     `yokl_yoklKL` (line "  - " `j'`) `o` Fore
+   `yukl` until `yii` input `yo` key
+     `yokl` (pass `a` State `aaa` scroll @List @Task `aaa` Up `rf` Down)
+       `rf` (pass `a` State `aaa` switch @Status `oo_a` sub @Only `o'` has)
+ `yi_yi'_yi'` initial `u` as @(Nonempty List) `u` transform @(Scrolling List)
