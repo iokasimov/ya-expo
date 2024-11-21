e8e9c09c004679ac16205de428d690cb7d8e4884
Murat Kasimov <iokasimov.m@gmail.com>
2024-11-21 10:05:27 +0700

    Update stack machine case

--- Ya/Case/stack-machine-1.hs
+++ Ya/Case/stack-machine-1.hs
@@ -5,10 +5,7 @@ import Ya.Expo.Instances
 import Ya.Expo.Terminal
 
 import "base" GHC.Num (Integer, (+))
-import "base" Data.String (String)
-import "base" Data.List ((++))
-import "base" Text.Show (Show (show))
-import "base" System.IO (IO, putStr)
+import "base" System.IO (IO)
 
 type Immediate v = v
 
@@ -20,28 +17,24 @@ pattern Immediate x = This x :: Command v
 pattern Operation x = That x :: Command v
 
 load v = enter @(State `WR` List _ `JNT` Halts)
- `yuk_` State `ha` Transition `he` push @List v
+ `yuk_` New `ha` State `ha` Transition `he` push @List v
 
 eval op = enter @(State `WR` List _ `JNT` Halts)
- `yuk_` State `ha` Transition `he` pop @List
- `lu'yp` State `ha` Transition `he` pop @List
- `yok_` Maybe `ha` (`yip'yo` op)
- `yok_` State `ha` Transition `ha` push @List
+ `yuk_` New `ha` State `ha` Transition `he` pop @List
+ `lu'yp` New `ha` State `ha` Transition `he` pop @List
+ `yok_` Try `ha` Maybe `ha` (`yip'yo` op)
+ `yok_` New `ha` State `ha` Transition `ha` push @List
 
-trace x = Forward @List "[OK] Trace: " `yokl` output
- `yuk__` x `yokl` integer `ho` Forward `ho'yokl` output `ho'yuk` output (Caret Space) `ho'yu` ()
+initial = Construct
+ `ha_` Next `he` Immediate 1
+ `ha_` Next `he` Immediate 2
+ `ha_` Next `he` Operation ((+) `hj`)
+ `ha_` Next `he` Immediate 4
+ `he_` Last `he` Operation ((+) `hj`)
 
-print' = is `hu` Forward "[ERROR] No operands!" `ho'yokl` output `la` this `he'ho` trace
+main = (initial `yokl` Forth `ha__` load `la` eval) `he'he` Empty @List Unit
+ `yi__` is `hu` "[ERR] No operands!" `ho_'yokl` Forth `ha` output `la_` this `he'ho` trace where
 
-example = as @(Nonempty List) @(Command Integer) `he__` is
- `li` Immediate 1
- `lu` Immediate 2
- `lu` Operation ((+) `hj`)
- `lu` Immediate 4
- `lu` Operation ((+) `hj`)
-
-main = print'
- `he____` Forward @(Nonempty List) example
-   `yokl` load `ha` is @(Immediate Integer)
-     `la` eval `ha` is @(Operation Integer)
-  `he___'he` Empty @List ()
+ trace x = is @(Nonempty List ASCII) "[OK] Trace: " `yokl` Forth `ha` output
+  `yuk___` New `he____` x `yokl` Forth `ha__` integer `ho_'yokl` Forth `ha` output
+  `ho_'yuk` New `ha` output `he` Caret Space
