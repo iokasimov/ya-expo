3aaa5410439891f77d53277b2ad399043289f821
Murat Kasimov <iokasimov.m@gmail.com>
2024-12-04 13:06:54 +0700

    Update balanced-brackets-1 source

--- Ya/Case/balanced-brackets-1.hs
+++ Ya/Case/balanced-brackets-1.hs
@@ -1,38 +1,30 @@
-import Ya
-import Ya.ASCII
-import Ya.Expo.Instances ()
-import Ya.Expo.ASCII
-import Ya.Expo.Terminal
+import "ya" Ya
+import "ya-ascii" Ya.ASCII
+import "ya-console" Ya.Console
+
+import "ya-expo" Ya.Expo.Instances
 
 import "base" System.IO (print)
 
-type Curved = ()
+type Parenthesis = Unit `ML` Unit
 
-type Parenthesis = Curved `ML` Curved
+deposit p = enter @(State `WR` List _ `JNT` Halts)
+ `yuk__` New `ha` State `ha` Event `he` push @List p
 
--- remember p = enter
- -- @(State `TI` List Curved `JT` Error Parenthesis)
- -- `yuk` push @List p `hu` State
+analyze _ = enter @(State `WR` List _ `JNT` Halts)
+ `yuk__` New `ha` State `ha` Event `he` pop @List
+ `yok__` Try `ha__` Error `hv` None `la` Valid `hv` Some
 
--- analyze _ = enter
- -- @(State `TI` List Curved `JT` Error Parenthesis)
- -- `yi'yuk` pop @List `hu` State
- -- `yi'yok` Error `ha` Closed `rfz` Valid
+remnant = is `ho'he` that @(List Unit)
+ `ho__` Valid `hv` Empty @List
+   `la` Error `hv'he` Nonempty @List
 
-example =
- is @Parenthesis
- `li` Opened ()
- -- `lu` Opened ()
- `lu` Closed ()
- -- `lu` Opened ()
- -- `lu` Closed ()
-
-main = print "typechecked"
-
--- main = (print `ha` but "[ERROR] Lonely bracket"
- -- `yi'rf` print `ha` but "[ERROR] Mismatching brackets"
- -- `yi'rfz` print `ha` but "[OKAY] Brackets are balanced")
- -- `yi'yi'yi'yi` (example `hu` as @(Nonempty List) @Parenthesis
- -- `yokl` way @Fore `haaa` remember `rf` analyze
- -- `_yiiiii` Empty @List ()
- -- `yi'yokl` Ok `_yi'rfz` Error `ha` Opened `ha` this `ha` unwrap top `yi'cnz'yp` Ok)
+main = Nonempty @List @Parenthesis
+ `ha` Next (Opened ()) `he` Last (Closed ())
+ `yokl` Forth `ha__` deposit `la` analyze
+ `he___'he` Empty @List ()
+ `yok_` Try `ha` remnant
+ `yi__` is @(List ASCII)
+ `ha__` is `hu` "[ERROR] A lonery or a missing bracket!"
+   `la` is `hu` "[VALID] Everything is seem to be good!"
+ `yokl` Forth `ha` output
