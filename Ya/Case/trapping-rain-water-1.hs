d29c9f6cb44c177cab10803674dafdb1760d47a8
Murat Kasimov <iokasimov.m@gmail.com>
2025-01-03 15:50:40 +0700

    Refactor trapping rain water tutorial

--- Ya/Case/trapping-rain-water-1.hs
+++ Ya/Case/trapping-rain-water-1.hs
@@ -11,11 +11,12 @@ gap = (trap `hj'hj`) where
 
  trap left right origin all = min left right - origin + all
 
-main = is `ho_'yokl` gap `ho'ho` auto `ho` Event `ho` State `ho` New `ho` Forth `ho_'he'he'hv` 0 `ho_` this
- `ha___` is `ho_'yokl` max `ho'ho` auto `ho` Event `ho` State `ho` New `ho` Forth `ho_'he'he'hv` 0 `ho_` this
- `lo_'yp` is `ho_'yokl` max `ho'ho` auto `ho` Event `ho` State `ho` New `ho` Prior `ho_'he'he'hv` 0 `ho_` this
- `lo_'yp` is @(Nonempty List Integer)
- `hv___` Nonempty @List
+tap = is `ho'yokl` (gap `ho'ho` auto `ho` Event `ho` State `ho` New `ho` Forth) `ho'he'he'hv` 0 `ho` this
+ `ha__` is `ho'yokl` (max `ho'ho` auto `ho` Event `ho` State `ho` New `ho` Forth) `ho'he'he'hv` 0 `ho` this
+ `lo'yp` is `ho'yokl` (max `ho'ho` auto `ho` Event `ho` State `ho` New `ho` Prior) `ho'he'he'hv` 0 `ho` this
+ `lo'yp` is @(Nonempty List Integer)
+
+main = tap `hv_` Nonempty @List
  `ha` Item 2 `ha` Maybe `ha` Next
  `ha` Item 5 `ha` Maybe `ha` Next
  `ha` Item 1 `ha` Maybe `ha` Next
@@ -26,3 +27,4 @@ main = is `ho_'yokl` gap `ho'ho` auto `ho` Event `ho` State `ho` New `ho` Forth 
  `ha` Item 7 `ha` Maybe `ha` Next
  `ha` Item 6 `ha` Maybe `hv` Last
  `yokl_` Forth `ha` Run `ha` print
+
