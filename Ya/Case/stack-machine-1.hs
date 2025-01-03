43ce58506e8a3d348c2c0ae34d73c10cdfac2a85
Murat Kasimov <iokasimov.m@gmail.com>
2025-01-03 13:56:34 +0700

    Refactor stack-machine tutorial

--- Ya/Case/stack-machine-1.hs
+++ Ya/Case/stack-machine-1.hs
@@ -1,11 +1,9 @@
 import "ya" Ya
+import "ya-world" Ya.World
 import "ya-ascii" Ya.ASCII
 import "ya-console" Ya.Console
 
-import Ya.Expo.Instances
-
 import "base" GHC.Num (Integer, (+))
-import "base" System.IO (IO)
 
 type Immediate v = v
 
@@ -13,16 +11,16 @@ type Operation v = v `LM` v `AR_` v
 
 type Command v = Immediate v `ML` Operation v
 
-pattern Immediate x = This x :: Command v
-pattern Operation x = That x :: Command v
+pattern Immediate x = This x
+pattern Operation x = That x
 
-load v = enter @(State `WR` List _ `JNT` Halts)
- `yuk_` New `ha` State `ha` Transition `hv` push @List v
+load value = enter @(State `WR` List _ `JNT` Halts)
+ `yuk_` New `ha` State `ha` Transition `hv` push @List value
 
-eval op = enter @(State `WR` List _ `JNT` Halts)
+eval binop = enter @(State `WR` List _ `JNT` Halts)
  `yuk_` New `ha` State `ha` Transition `hv` pop @List
  `lu'yp` New `ha` State `ha` Transition `hv` pop @List
- `yok_` Try `ha` Maybe `ha` (`yip'yo` op)
+ `yok_` Try `ha` Maybe `ha` (`yip'yo` binop)
  `yok_` New `ha` State `ha` Transition `ha` push @List
 
 initial = Construct
@@ -32,9 +30,12 @@ initial = Construct
  `ha` (Item `hv` Immediate 4) `ha` Maybe `ha` Next
  `ha` (Item `hv` Operation ((+) `hj`)) `ha` Maybe `hv` Last
 
-main = (initial `yokl` Forth `ha` Run `ha__` load `la` eval) `he'he` Empty @List Unit
- `yi__` is `hu` "[ERR] No operands!" `ho_'yokl` Forth `ha` Raw `ha` output `la_` this `he'ho` trace where
+main = error `la` this `he'ho` trace
+ `hv_______` initial `yokl` Forth `ha` Run `ha__` load `la` eval
+ `he'he'hv___` Empty @List Unit where
+
+ error _ = "[ERR] No operands!" `yokl` Forth `ha` Raw `ha` output
 
  trace x = is @(Nonempty List ASCII) "[OK] Trace: " `yokl` Forth `ha` Raw `ha` output
-  `yuk___` Raw `hv____` x `yokl` Forth `ha`Run `ha__` integer `ho_'yokl` Forth `ha` Raw `ha` output
+  `yuk___` Raw `hv____` x `yokl` Forth `ha` Run `ha__` integer `ho_'yokl` Forth `ha` Raw `ha` output
   `ho_'yuk` Raw `ha` output `hv` Caret Space
