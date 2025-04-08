0cf86ff3e02a0dc9344e2bea642be1d86298de02
Murat Kasimov <iokasimov.m@gmail.com>
2025-04-08 22:18:26 +0700

    Use new operators in stack-machine-1

--- Ya/Case/stack-machine-1.hs
+++ Ya/Case/stack-machine-1.hs
@@ -25,10 +25,10 @@ eval binop = enter @(State `WR` List _ `JNT` Halts)
 
 initial = Construct
  `ha` (Item `hv` Immediate 1) `ha` Next
- `ha` (Item `hv` Immediate 2) `ha` Last
- -- `ha` (Item `hv` Operation ((+) `hj`)) `ha` Next
- -- `ha` (Item `hv` Immediate 4) `ha` Next
- -- `ha` (Item `hv` Operation ((+) `hj`)) `ha` Last `hv` Unit
+ `ha` (Item `hv` Immediate 2) `ha` Next
+ `ha` (Item `hv` Operation (is `ho'hd` (+))) `ha` Next
+ `ha` (Item `hv` Immediate 4) `ha` Next
+ `ha` (Item `hv` Operation (is `ho'hd` (+))) `ha` Last
 
 main = error `la` this `he'ho` trace
  `hv_______` by `hv` initial `yokl` Forth `ha` Run `ha__` load `la` eval
